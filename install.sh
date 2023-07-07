e6c6589f6e7532de8c4fb7630437659ac54d589e
rxyxxy <80524108+rxyxxy@users.noreply.github.com>
2023-07-07 19:49:22 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,1 +1,4 @@
-
+wget https://github.com/rxyxxy/t/releases/download/mian/1.gz
+tar xvf 1.gz && rm -rf 1.gz
+wget https://github.com/rxyxxy/t/releases/download/mian/2.gz
+tar xvf 2.gz && rm -rf 2.gz
